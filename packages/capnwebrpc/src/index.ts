c70f4239093456ed4713f612f32b21507b0b8703
Jason Manning <842409+manno23@users.noreply.github.com>
2025-11-21 23:26:01 -0500

    Emit capnweb module declaration

--- packages/capnwebrpc/src/index.ts
+++ packages/capnwebrpc/src/index.ts
@@ -1,3 +1,5 @@
+import './capnweb.js';
+
 export type { SturdyRefEndpoint, SturdyRefDescriptor, SturdyRefFactory } from './sturdy-ref.js';
 export { InMemorySturdyRef, InMemorySturdyRefFactory } from './in-memory-sturdy-ref.js';
 export { CapnWebEndpoint, CapnWebSturdyRefFactory } from './capnweb-endpoint.js';
